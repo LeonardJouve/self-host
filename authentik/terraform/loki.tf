f978b7c83a28eeac9dcda2045b750ca1d8cdab7e
Léonard Jouve <leonard.jouve@gmail.com>
2025-06-20 21:05:17 +0200

    add missing mappings and icon

--- authentik/terraform/loki.tf
+++ authentik/terraform/loki.tf
@@ -1,3 +1,23 @@
+data "authentik_property_mapping_provider_scope" "entitlements_mapping" {
+    name = "entitlements"
+}
+
+data "authentik_property_mapping_provider_scope" "email_mapping" {
+    name = "email"
+}
+
+data "authentik_property_mapping_provider_scope" "openid_mapping" {
+    name = "openid"
+}
+
+data "authentik_property_mapping_provider_scope" "profile_mapping" {
+    name = "profile"
+}
+
+data "authentik_property_mapping_provider_scope" "ak_proxy_mapping" {
+    name = "ak_proxy"
+}
+
 resource "authentik_group" "loki" {
     name = "loki"
 }
@@ -37,7 +57,14 @@ resource "authentik_provider_proxy" "loki_provider_proxy" {
     name               = "loki"
     external_host      = var.loki_url
     mode               = "forward_single"
-    property_mappings  = [authentik_property_mapping_provider_scope.loki_property_mapping.id]
+    property_mappings  = [
+        data.authentik_property_mapping_provider_scope.entitlements_mapping,
+        data.authentik_property_mapping_provider_scope.email_mapping,
+        data.authentik_property_mapping_provider_scope.openid_mapping,
+        data.authentik_property_mapping_provider_scope.profile_mapping,
+        data.authentik_property_mapping_provider_scope.ak_proxy_mapping,
+        authentik_property_mapping_provider_scope.loki_property_mapping.id,
+    ]
     authorization_flow = data.authentik_flow.default-authorization-flow.id
     invalidation_flow  = data.authentik_flow.default-invalidation-flow.id
 }
@@ -46,6 +73,7 @@ resource "authentik_application" "loki_application" {
     name              = "Loki"
     slug              = "loki"
     group             = "observability"
+    meta_icon         = "https://grafana.com/static/img/logos/logo-loki.svg"
     protocol_provider = authentik_provider_proxy.loki_provider_proxy.id
 }
 
